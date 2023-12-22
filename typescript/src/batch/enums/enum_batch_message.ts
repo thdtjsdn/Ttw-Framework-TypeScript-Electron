4181674cf15c14e0ada05bdad5d624604b99f368
thdtjsdn <thdtjsdn@gmail.com>
2023-12-22 14:17:58 +0900

    Create enum_batch_message.ts

---
+++ typescript/src/batch/enums/enum_batch_message.ts
@@ -0,0 +1,5 @@
+export enum EnumBatchMessage {
+	  MSG_COLLECT_COMPLETE_PAGE = 'Page collection complete - restart in 10 minutes'
+	, MSG_COLLECT_STATUS_PAGE = 'Page collection status'
+	, MSG_ERROR_502 = 'ResponseStatus(502) - Restart in 10 minutes'
+}
